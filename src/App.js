65617f4ec964448d36e7dc8d67e001a65b682f40
dudafmeneses <mariaeduarda.meneses2608@gmail.com>
2023-07-12 17:36:44 -0300

    push projeto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import {FiSearch} from 'react-icons/fi';
+import { useState } from 'react';
+import './styles.css';
+import api from './app/api';
 
 function App() {
+
+  const [input, setInput] = useState();
+  const [id, setId] = useState({})
+
+  async function handleSearch(){
+
+    const min = input.toLowerCase();
+    const rep = min.split(" ").join("-")
+
+    if(input === ""){
+      alert("Preencha o campo!")
+      return;
+    }
+    const response = await api.get(`${rep.normalize('NFD').replace(/[\u0300-\u036f]/g, "")}`)
+    setId(response.data)
+    setInput("")
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="containter">
+        <h1 className="title">BUSCADOR DE ID IBGE</h1>
+
+        <div className="containerInput">
+          <input
+          type="text"
+          placeholder="Digite a cidade"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          />
+
+        <button className="buttonSearch" onClick={handleSearch}>
+          <FiSearch size={25} color='black'/>
+        </button>
+        </div>
+
+        <main className='main'>
+          <span>Cidade: {id.nome}</span>
+          <span>Id: {id.id}</span>
+        </main>
+
     </div>
   );
 }
